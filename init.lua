0143bbee4e19f0052f874b9e40ea13bc7fc8dc73
Your Name <hieu.dao96@gmail.com>
2025-04-06 16:34:30 +0200

    add indent blank

--- init.lua
+++ init.lua
@@ -5,9 +5,3 @@ require 'config.options'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-
--- require 'kickstart.plugins.debug'
--- require 'kickstart.plugins.indent_line',
--- require 'kickstart.plugins.lint',
--- require 'kickstart.plugins.autopairs',
--- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
