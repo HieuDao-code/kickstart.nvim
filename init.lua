55510ae18bd87aa4663c969dace99b0e709e6ccc
Your Name <hieu.dao96@gmail.com>
2025-04-06 11:57:42 +0200

    remove neo and few changes

--- init.lua
+++ init.lua
@@ -1,25 +1,25 @@
 require 'config.lazy'
 
 require('lazy').setup('hieu.plugins', {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+  -- ui = {
+  --   -- If you are using a Nerd Font: set icons to an empty table which will use the
+  --   -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+  --   icons = vim.g.have_nerd_font and {} or {
+  --     cmd = '⌘',
+  --     config = '🛠',
+  --     event = '📅',
+  --     ft = '📂',
+  --     init = '⚙',
+  --     keys = '🗝',
+  --     plugin = '🔌',
+  --     runtime = '💻',
+  --     require = '🌙',
+  --     source = '📄',
+  --     start = '🚀',
+  --     task = '📌',
+  --     lazy = '💤 ',
+  --   },
+  -- },
 })
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
@@ -146,20 +146,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
--- init.lua. If you want these files, they are in the repository, so you can just download them and
--- place them in the correct locations.
-
--- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
---
---  Here are some example plugins that I've included in the Kickstart repository.
---  Uncomment any of the lines below to enable them (you will need to restart nvim).
---
 -- require 'kickstart.plugins.debug'
 -- require 'kickstart.plugins.indent_line',
 -- require 'kickstart.plugins.lint',
 -- require 'kickstart.plugins.autopairs',
--- require 'kickstart.plugins.neo-tree',
 -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
 -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
--- lua/hieu/plugins/treesitter.lua
+++ lua/hieu/plugins/treesitter.lua
@@ -3,6 +3,14 @@ return {
   'nvim-treesitter/nvim-treesitter',
   -- Make sure a parser is at the latest compatible version
   build = ':TSUpdate',
+  dependencies = {
+    {
+      'nvim-treesitter/nvim-treesitter-context',
+    },
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+  },
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
@@ -14,11 +22,66 @@ return {
       additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn', -- set to `false` to disable one of the mappings
+        node_incremental = 'grn',
+        scope_incremental = 'grc',
+        node_decremental = 'grm',
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        -- Automatically jump forward to textobj, similar to targets.vim
+        lookahead = true,
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['af'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
+          ['if'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
+          ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
+          ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+          ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
+          ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
+        },
+        include_surrounding_whitespace = true,
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>na'] = '@parameter.inner', -- swap parameters/argument with next
+        ['<leader>nm'] = '@function.outer', -- swap function with next
+      },
+      swap_previous = {
+        ['<leader>pa'] = '@parameter.inner', -- swap parameters/argument with prev
+        ['<leader>pm'] = '@function.outer', -- swap function with previous
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = { query = '@function.outer', desc = 'Next method/function def start' },
+        [']c'] = { query = '@class.outer', desc = 'Next class start' },
+        [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+      },
+      goto_next_end = {
+        [']M'] = { query = '@function.outer', desc = 'Next method/function def end' },
+        [']C'] = { query = '@class.outer', desc = 'Next class end' },
+        [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
+      },
+      goto_previous_start = {
+        ['[m'] = { query = '@function.outer', desc = 'Prev method/function def start' },
+        ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
+        ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
+      },
+      goto_previous_end = {
+        ['[M'] = { query = '@function.outer', desc = 'Prev method/function def end' },
+        ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
+        ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
+      },
+    },
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
